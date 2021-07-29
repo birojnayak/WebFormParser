79aed6aa218912ad9023c012914facad63fee29e
Biroj Nayak <49173255+birojnayak@users.noreply.github.com>
2021-07-29 11:27:15 -0700

    added events tracking from codebehind

--- ASPXParser/Program.cs
+++ ASPXParser/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using HtmlAgilityPack;
 
 namespace ASPXParser
@@ -45,29 +46,56 @@ namespace ASPXParser
         {
             try
             {
-                string[] allaspxFiles = Directory.GetFiles(directoryPath, "*.aspx", SearchOption.AllDirectories);
-                var files = Directory.EnumerateFiles(directoryPath, "*.*", SearchOption.AllDirectories)
-            .Where(s => s.EndsWith(".aspx") || s.EndsWith(".ascx") || s.EndsWith(".master"));
+                var allFiles = Directory.EnumerateFiles(directoryPath, "*.*", SearchOption.AllDirectories)
+            .Where(s => s.EndsWith(".aspx") || s.EndsWith(".aspx.cs") || s.EndsWith(".ascx") || s.EndsWith(".ascx.cs") || s.EndsWith(".master") || s.EndsWith(".master.cs"))
+            .OrderByDescending(filename => filename);
+
                 ControlsData controls = new ControlsData();
                 HtmlAgilityPack.HtmlDocument htmlDoc = new HtmlAgilityPack.HtmlDocument();
                 htmlDoc.OptionFixNestedTags = true;
-                foreach (String file in allaspxFiles)
+                List<string> allListOfFiles = allFiles.ToList();
+                for(int i=0; i< allListOfFiles.Count; i++)
                 {
-                    htmlDoc.Load(file);
+                    if (allListOfFiles[i].EndsWith(".cs")) // since we have processed already
+                    {
+                        continue;
+                    }
+
+                    //get all codebehind events for a file
+                    List<string> codeBehindEventLines = null;
+                    if ( allListOfFiles[i-1].EndsWith(".cs"))
+                    {
+                        codeBehindEventLines = getCodeBehindEventsLines(allListOfFiles[i - 1]);
+
+                    }
+
+                    htmlDoc.Load(allListOfFiles[i]);
                     var nodes = htmlDoc.DocumentNode.Descendants();
                     foreach (HtmlNode nd in nodes)
                     {
                         List<string> attributes = new List<string>();
+                        bool isServer = false;
+                        string id = "";
                         foreach (HtmlAttribute att in nd.Attributes)
                         {
-                            if (att.Name.Contains("runat", StringComparison.OrdinalIgnoreCase) || att.Name.Contains("id", StringComparison.OrdinalIgnoreCase))
+                            if (att.Name.Contains("runat", StringComparison.OrdinalIgnoreCase))
                             {
+                                isServer = true;
+                                continue;
+                            }
+                            if(att.Name.Contains("id", StringComparison.OrdinalIgnoreCase))
+                            {
+                                id = att.Value;
                                 continue;
                             }
                             attributes.Add(att.Name);
 
                         }
-                       controls.AddControl(nd.Name, attributes.ToArray());
+                        if(isServer && id.Length > 0 && codeBehindEventLines != null && codeBehindEventLines.Count>0)
+                        {
+                           getCodeBehindEvents(codeBehindEventLines, id, attributes);
+                        }
+                        controls.AddControl(nd.Name, attributes.ToArray());
                     }
                 }
                 return controls;
@@ -93,6 +121,57 @@ namespace ASPXParser
         private static bool isValidPath(String directoryPath)
         {
             return Directory.Exists(directoryPath);
+        }
+
+        private static List<string> getCodeBehindEventsLines(String filePath)
+        {
+            String pattern = ".+=.*EventHandler\\(.*\\)";
+            List<string> serverEvents = new List<string>();
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null && !line.StartsWith("//"))
+                    {
+                        Match m = Regex.Match(line, pattern, RegexOptions.IgnoreCase);
+                        if (m.Success)
+                        {
+                            serverEvents.Add(line);
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return serverEvents;
+        }
+
+        private static void getCodeBehindEvents(List<String> allLines, String id, List<string> attributes)
+        {
+            foreach (String line in allLines)
+            {
+                if (line.Contains( id + ".") && line.Contains(id+"_"))
+                {
+                    String fullLine = line.Trim();
+                    int startIndex = fullLine.IndexOf(id + ".");
+                    int endIndex = fullLine.IndexOf("+=");
+                    int position = startIndex + id.Length + 1;
+                    if (startIndex >= 0 && endIndex > startIndex && endIndex> position)
+                    {
+                        string attr = fullLine.Substring(position, endIndex- position);
+                        if (!string.IsNullOrEmpty(attr))
+                        {
+                            attributes.Add(attr.Trim());
+                        }
+                    }
+                }
+            }
         }
     }
 }
